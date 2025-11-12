379b221837ca584507edf1be383946547bb375e5
gilli-jonathan <gilli.jonathan97@gmail.com>
2025-11-12 14:38:25 +0100

    working whit card component

--- src/App.jsx
+++ src/App.jsx
@@ -23,23 +23,7 @@ function App() {
       <h1 className="my-5">ATTORI DI HOLLYWOOD</h1>
 
       {arrAttrici.map((obj) => (
-        <div className="card" style={{ width: "18rem" }}>
-          <img src={obj.image} className="card-img-top" alt="..." />
-          <div className="card-body">
-            <h5 className="card-title">Nome</h5>
-            <p className="card-text">{obj.name}</p>
-            <h6 className="card-title">anno di nascita</h6>
-            <p className="card-text">{obj.birth_year}</p>
-            <h6 className="card-title">nazionalità</h6>
-            <p className="card-text">{obj.nationality}</p>
-            <h6 className="card-title">biografia</h6>
-            <p className="card-text">{obj.biography}</p>
-            <h6 className="card-title">premi</h6>
-            <p className="card-text">{obj.awards}</p>
-          </div>
-        </div>
-
-
+        <Card key={obj.id} obj={obj} />
       ))}
 
 
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,4 +1,4 @@
-export default function Card(obj) {
+export default function Card({ obj }) {
 
 
 
