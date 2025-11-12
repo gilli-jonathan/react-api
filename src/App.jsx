de821687142c5ecec2ed4c905da1728aef3952d7
gilli-jonathan <gilli.jonathan97@gmail.com>
2025-11-12 14:28:52 +0100

    create component for card

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
 import { useEffect, useState } from "react"
 import axios from "axios"
+import Card from "./Card"
 
 function App() {
 
   const attrici = "https://lanciweb.github.io/demo/api/actresses/"
+  const [arrAttrici, setArrAttrici] = useState([])
 
   const attori = "https://lanciweb.github.io/demo/api/actresses/"
+  const [arrAttori, setArrAttori] = useState([])
 
 
   axios.get(attrici)
-    .then((resp) => {
-      console.log(resp.data)
-    });
-
-
-
-
+    .then((resp) => { setArrAttrici(resp.data) });
 
 
   return (
     <>
       <h1 className="my-5">ATTORI DI HOLLYWOOD</h1>
 
-      <div className="card" style={{ width: "18rem" }}>
-        <img src="..." className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      {arrAttrici.map((obj) => (
+        <Card obj={obj} />
 
-        </div>
-      </div>
+      ))}
+
+
 
     </>
   )
---
+++ src/Card.jsx
@@ -0,0 +1,23 @@
+export default function Card(obj) {
+
+
+
+    return (
+        <div key={obj.id} className="card" style={{ width: "18rem" }}>
+            <img src={obj.image} className="card-img-top" alt="..." />
+            <div className="card-body">
+                <h5 className="card-title">Nome</h5>
+                <p className="card-text">{obj.name}</p>
+                <h6 className="card-title">anno di nascita</h6>
+                <p className="card-text">{obj.birth_year}</p>
+                <h6 className="card-title">nazionalità</h6>
+                <p className="card-text">{obj.nationality}</p>
+                <h6 className="card-title">biografia</h6>
+                <p className="card-text">{obj.biography}</p>
+                <h6 className="card-title">premi</h6>
+                <p className="card-text">{obj.awards}</p>
+            </div>
+        </div>
+
+    )
+}
